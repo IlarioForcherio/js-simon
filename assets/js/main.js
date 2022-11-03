9b5a2ce3e31d9c215622f8c2f05df2313be9ca39
ilarioforcherio <ilarioforcherio@gmail.com>
2022-11-03 16:56:06 +0100

    start project + ragionamento

---
+++ assets/js/main.js
@@ -0,0 +1,78 @@
+
+
+
+// let buttonHtml = document.getElementById('button-html');
+ 
+// //la funzione setTimeout fa partire un blocco di codice dopo un certo tempo 
+// //al posto di mettere function e la funzione dentro le graffe, creo una funzione da passare nel parametro function 
+
+// //let prova = setTimeout(function(){},2000)
+
+
+// //versione con function richiamata nel parametro 
+// //let prova = setTimeout(saluta,2000);
+
+
+// function saluta(){
+//     alert ('ciao'); 
+// }
+
+// //con l'event listener metto "in ascolto" il bottone "button-html"
+// //il corpo della funzione partirà al click
+// buttonHtml.addEventListener('click' , function(){
+// //versione con function richiamata nel parametro 
+
+// let prova = setTimeout(saluta,2000);
+
+// })
+
+
+
+
+
+// Descrizione:
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+//crea un array vuoto di numeri
+//crea funzione numeri casuali
+//crea 5 numeri casuali con un ciclo
+//pushali nell'array
+//visualizzali in pagina 
+//fai partire un timer di 30 secondi 
+//chiedi all'utente di inserire 5 numeri(con un ciclo) di quelli che si ricorda 
+//questi numeri verranno pushati in un array
+//vedere se i numeri inseriti dall'utente sono preseti nell'array niziale se si incrementare di uno un counter punteggio 
+//i numeri giusti finiscono in un array 
+
+//numeri
+let randomNumber = [];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
